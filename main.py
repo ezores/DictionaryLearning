47f9d3e56c5a42a86d71e15ad15e0a7931691a8d
Cycle <53787848+ezores@users.noreply.github.com>
2022-01-30 06:41:48 +0000

    Formatted the output for multiple outputs

--- main.py
+++ main.py
@@ -4,5 +4,25 @@
 import json as js
 
 data = js.load(open("data.json"))
+#print(data) # testing if it works
 
-print(data) # testing if it works
+def translate(toSearch):
+  toSearch = toSearch.lower()
+  if toSearch in data:
+    return data[toSearch]
+  elif toSearch.title() in data:
+    return data[toSearch.title()]
+  elif toSearch.upper() in data:
+    return data[toSearch.upper()]
+  else:
+    print("The entered word does not match our records! Please try again ")
+
+
+
+toSearch = input("Enter the word you would like to search: ")
+output = translate(toSearch)
+if type(output) == list:
+  for item in output:
+    print(item)
+else:    
+  print(output)
