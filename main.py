63fdaa890a2004e9a2b9c89851a821d83b58a718
Cycle <53787848+ezores@users.noreply.github.com>
2022-01-30 06:47:43 +0000

    Project done, works

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-#Simple dictionary program to help me understand Python
+import json
+from difflib import get_close_matches
 
-#data.json contains the dictionary data to use for the program
-import json as js
-import difflib
-data = js.load(open("data.json"))
-#print(data) # testing if it works
+data = json.load(open("data.json"))
 
-def translate(toSearch):
-  toSearch = toSearch.lower()
-  if toSearch in data:
-    return data[toSearch]
-  elif toSearch.title() in data:
-    return data[toSearch.title()]
-  elif toSearch.upper() in data:
-    return data[toSearch.upper()]
-  elif len(get_close_matches(toSearch, data.keys())) > 0 :
-      print("Did you mean %s instead of: " %get_close_matches(toSearch,data.keys())[0])
-  else:
-    print("The entered word does not match our records! Please try again ")
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif len(get_close_matches(word , data.keys())) > 0 :
+        print("did you mean %s instead" %get_close_matches(word, data.keys())[0])
+        decide = input("press y for yes or n for no")
+        if decide == "y":
+            return data[get_close_matches(word , data.keys())[0]]
+        elif decide == "n":
+            return("pugger your paw steps on wrong keys ")
+        else:
+            return("You have entered wrong input please enter just y or n")
+    else:
+        print("pugger your paw steps on wrong keys")
 
 
 
-toSearch = input("Enter the word you would like to search: ")
-output = translate(toSearch)
+word = input("Enter the word you want to search")
+output = translate(word)
 if type(output) == list:
-  for item in output:
-    print(item)
-else:    
-  print(output)
+    for item in output:
+        print(item)
+else:
+    print(output)
