1bd3f53cc8727c78969d011c638e871db43f19e4
Cycle <53787848+ezores@users.noreply.github.com>
2022-01-30 06:48:46 +0000

    Spaces

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def translate(word):
         return data[word.upper()]
     elif len(get_close_matches(word , data.keys())) > 0 :
         print("did you mean %s instead" %get_close_matches(word, data.keys())[0])
-        decide = input("press y for yes or n for no")
+        decide = input("press y for yes or n for no: ")
         if decide == "y":
             return data[get_close_matches(word , data.keys())[0]]
         elif decide == "n":
             return("pugger your paw steps on wrong keys ")
         else:
-            return("You have entered wrong input please enter just y or n")
+            return("You have entered wrong input please enter just y or n: ")
     else:
         print("pugger your paw steps on wrong keys")
 
