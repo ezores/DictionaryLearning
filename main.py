5000ab4ca96f6807ddf5aaeb0c24c2cdaa57fe0d
Cycle <53787848+ezores@users.noreply.github.com>
2022-01-30 06:22:14 +0000

    Successfully added the data.json to my program

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #Simple dictionary program to help me understand Python
+
+#data.json contains the dictionary data to use for the program
+import json as js
+
+data = js.load(open("data.json"))
+
+print(data) # testing if it works
